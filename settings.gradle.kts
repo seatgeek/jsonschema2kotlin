5e19901911ab4afbcd04ae468c7a03f3c42d5316
Dallas Gutauckis <dallas@gutauckis.com>
2021-05-29 23:41:19 -0400

    Removing gradle plugin for now

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ rootProject.name = "jsonschema2kotlin"
 
 include("jsonschema2kotlin-cli")
 include("jsonschema2kotlin-lib")
-include("jsonschema2kotlin-gradle-plugin")
+// include("jsonschema2kotlin-gradle-plugin")
